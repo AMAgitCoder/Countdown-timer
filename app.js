13cafbc18b0330dddcc394ed002a3acb5d663ca3
AMAN KUMAR MAHTO <aman.mahto.e21@nsut.ac.in>
2023-06-21 22:29:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const endDate = "23 June 2023 10:00 PM"
+const endDate = "23 June 2030 10:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
